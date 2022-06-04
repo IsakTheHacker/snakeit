32d4b084f23a08ee93bdb93c25fe28c0e91520a8
IsakTheHacker <67378443+IsakTheHacker@users.noreply.github.com>
2022-06-04 17:53:36 +0200

    Change snake appearance

--- src/header/stdconfig.h
+++ src/header/stdconfig.h
@@ -2,15 +2,15 @@
 #include <string>
 
 namespace stdconf {
-	//const wchar_t snakeBodyPart = L'ｏ';
-	//const wchar_t snakeHead = L'Ｏ';
-	//const wchar_t food = L'＊';
-	//const wchar_t emptySpace = L'ｦ';
+	const wchar_t snakeBodyPart = L'ｏ';
+	const wchar_t snakeHead = L'Ｏ';
+	const wchar_t food = L'＊';
+	const wchar_t emptySpace = L'ｦ';
 
-	const wchar_t snakeBodyPart = L'o';
-	const wchar_t snakeHead = L'O';
-	const wchar_t food = L'@';
-	const wchar_t emptySpace = L' ';
+	//const wchar_t snakeBodyPart = L'o';
+	//const wchar_t snakeHead = L'O';
+	//const wchar_t food = L'@';
+	//const wchar_t emptySpace = L' ';
 
 	const int xOffset = 2;
 	const int yOffset = 1;
